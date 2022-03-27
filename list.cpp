0769d124bc0aed8a73b97bcfb6c82cffe55ed83b
unknown <jerzygos@yahoo.co.uk>
2022-03-28 00:48:11 +0100

    JG 2nd resolved task 2

---
+++ list.cpp
@@ -0,0 +1,29 @@
+#include <list>
+#include <iostream>
+
+// version 2 to upload 
+
+List* initList(void)
+{
+    List* myList = (List*)malloc(sizeof(*myList));
+ 
+return mylist;
+}
+
+
+void listauczniow(list* myList)
+{
+    free(myList);
+    myList = NULL;
+}
+
+void  daneUczniow(List* myList, char* daneosob)
+{
+
+}
+int main() {
+
+    std::cout << "Hello from Jerzy 2 time";
+
+  return 0;
+}
