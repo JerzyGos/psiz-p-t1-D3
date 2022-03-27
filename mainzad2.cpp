99ea24886a78e7d4184bcd0157045cdd80ecd75e
JerzyGos <45019955+JerzyGos@users.noreply.github.com>
2022-03-27 23:26:14 +0100

    Add files via upload

---
+++ mainzad2.cpp
@@ -0,0 +1,32 @@
+#include <list>
+#include <iostream>
+
+
+
+List* initList(void)
+{
+    List* myList = (List*)malloc(sizeof(*myList));
+    if(NULL == myList)
+  {
+    return NULL:
+  }
+return mylist;
+}
+
+
+void listauczniow(list* myList)
+{
+    free(myList);
+    myList = NULL;
+}
+
+void  daneUczniow(List* myList, char* daneosob)
+{
+
+}
+int main() {
+
+    std::cout << "Hello from Jerzy ";
+
+  return 0;
+}
